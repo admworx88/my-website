1a343e5a20d4c476bb04a39baac867372700ecad
Aljon Moliva <nojlamoliva@yahoo.com>
2022-09-15 10:45:27 +0800

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
-import {
-  Navbar,
-  Hero,
-  SocialLinks,
-  Portfolio,
-  Education,
-  About,
-  Contacts,
-  Footer,
-} from '../src/components/Index';
+import Navbar from '../src/components/Navbar';
+import Hero from '../src/components/Hero';
+import SocialLinks from '../src/components/SocialLinks';
+import Portfolio from '../src/components/Portfolio';
+import Education from '../src/components/Education';
+import About from '../src/components/About';
+import Contacts from '../src/components/Contacts';
+import Footer from '../src/components/Footer';
 
 export default function App() {
   return (
